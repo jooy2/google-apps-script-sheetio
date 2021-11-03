f52c460e0afdbeae2b76a22ce9d37052850c414f
leejooy96 <leejooy96@gmail.com>
2021-11-03 14:53:53 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-undef': 0,
+    'no-throw-literal': 0,
+    'no-unused-expressions': 1,
+    'no-unused-vars': 1,
+  },
+};
